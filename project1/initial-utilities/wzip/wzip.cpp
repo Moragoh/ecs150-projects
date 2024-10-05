d581be3703eece472195ec492ef8726d19327219
Moragoh <joshjun010202@gmail.com>
2024-10-05 11:19:40 -0700

    wzip passed

--- project1/initial-utilities/wzip/wzip.cpp
+++ project1/initial-utilities/wzip/wzip.cpp
@@ -1,40 +1,56 @@
 #include <string>
 #include <cstring>
-#include <iostream> // For debugging
+#include <iostream> 
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sstream>
+#include <bitset>
 using namespace std;
+
+int writeCompressionRes(uint32_t count, char charToWrite)
+{
+    // Write the count in binary
+    if ((write(STDOUT_FILENO, &count, 4)) < 0)
+        return -1;
+    // Write the character
+    if ((write(STDOUT_FILENO, &charToWrite, sizeof(charToWrite))) < 0)
+        return -1;
+
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
     int fileDescriptor;
     int ret;
     int writeRet;
-    char buffer[5];
+    char buffer[1];
     string str;
     stringstream stringOut;
+    stringstream totalInput;
 
-    if (argc == 1) {
-        stringOut << "wzip : file1[file2...]\n";
+    if (argc == 1)
+    {
+        stringOut << "wzip: file1 [file2 ...]\n";
         // Convert to cstring so that it can be written out to stdout
         str = stringOut.str();
-        
+
         // Error checking--no instructions given on what to do here
-        if (writeRet = write(STDOUT_FILENO, str.c_str(), str.length()) < 0)
+        if ((writeRet = write(STDOUT_FILENO, str.c_str(), str.length())) < 0)
         {
             return -1;
         }
 
         return 1;
-    } 
-    else 
+    }
+    else
     {
-        // Iterate through each file
+        // Iterate through each file and concatenate the results first
         for (int i = 1; i < argc; i++)
         {
             fileDescriptor = open(argv[i], O_RDONLY);
+
             // Error check on opening file
             if (fileDescriptor < 0)
             {
@@ -44,7 +60,7 @@ int main(int argc, char *argv[])
                 // Convert to cstring so that it can be written out to stdout
                 str = stringOut.str();
                 // Error checking--no instructions given on what to do here
-                if (writeRet = write(STDOUT_FILENO, str.c_str(), str.length()) < 0)
+                if ((writeRet = write(STDOUT_FILENO, str.c_str(), str.length())) < 0)
                 {
                     return -1;
                 }
@@ -53,21 +69,48 @@ int main(int argc, char *argv[])
             }
             else
             {
-                // Init prevChar to null
-
-                // Read each file
+                // Combine into one stringStream
                 while ((ret = read(fileDescriptor, buffer, sizeof(buffer))) > 0)
                 {
-                    // Read one byte at a time
-                    // If prevChar is null, set prevChar and increment charCount
-                    // elif prevChar and this char diff, connvert count to binary in 4 bits and add the character. Reset to 0
-                    // else same, increment
+                    totalInput << buffer[0];
                 }
             }
+            // Close file
+            close(fileDescriptor);
         }
-        // Close file
-        close(fileDescriptor);
-    }
 
+        // Perform the compression
+        string totalInputStr = totalInput.str();
+        // Init prevChar to null
+        char prevChar = '\0';
+        uint32_t charCount = 0; // Fixed to 4 bytes
+        // Iterate through the string character by character
+        for (int i = 0; i < totalInputStr.length(); i++)
+        {
+
+            // Check if prevChar is null (to see if this is the fist char)
+            if (prevChar == '\0')
+            {
+                prevChar = totalInputStr[i];
+                charCount++;
+            }
+            else if (totalInputStr[i] == prevChar)
+            {
+                charCount++;
+            }
+            else
+            {
+                // Write the compressed version of this string and reset the chaarCount since we encountered a new character
+                writeCompressionRes(charCount, prevChar);
+
+                // Reset
+                charCount = 1;
+                prevChar = totalInputStr[i];
+            }
+        }
+
+        // Special case for writing whatever was countiing when the file ends
+        writeCompressionRes(charCount, prevChar);
+    }
     return 0;
 }

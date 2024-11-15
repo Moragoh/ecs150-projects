b973ba5b4897433f21b485e77d05907756c7aca8
Moragoh <joshjun010202@gmail.com>
2024-11-15 04:04:51 +0000

    Comments added

--- project4/gunrock_web/LocalFileSystem.cpp
+++ project4/gunrock_web/LocalFileSystem.cpp
@@ -8,61 +8,74 @@
 
 using namespace std;
 
-
-LocalFileSystem::LocalFileSystem(Disk *disk) {
+LocalFileSystem::LocalFileSystem(Disk *disk)
+{
   this->disk = disk;
 }
 
 // Read super block and allocate the structure accordingly
-void LocalFileSystem::readSuperBlock(super_t *super) {
-
+void LocalFileSystem::readSuperBlock(super_t *super)
+{
 }
 
-void LocalFileSystem::readInodeBitmap(super_t *super, unsigned char *inodeBitmap) {
-
+void LocalFileSystem::readInodeBitmap(super_t *super, unsigned char *inodeBitmap)
+{
+  // Check if inode is written to
 }
 
-void LocalFileSystem::writeInodeBitmap(super_t *super, unsigned char *inodeBitmap) {
-
+void LocalFileSystem::writeInodeBitmap(super_t *super, unsigned char *inodeBitmap)
+{
 }
 
-void LocalFileSystem::readDataBitmap(super_t *super, unsigned char *dataBitmap) {
-
+void LocalFileSystem::readDataBitmap(super_t *super, unsigned char *dataBitmap)
+{
 }
 
-void LocalFileSystem::writeDataBitmap(super_t *super, unsigned char *dataBitmap) {
-
+void LocalFileSystem::writeDataBitmap(super_t *super, unsigned char *dataBitmap)
+{
 }
 
-void LocalFileSystem::readInodeRegion(super_t *super, inode_t *inodes) {
-
+void LocalFileSystem::readInodeRegion(super_t *super, inode_t *inodes)
+{
+  // Read inode
 }
 
-void LocalFileSystem::writeInodeRegion(super_t *super, inode_t *inodes) {
-
+void LocalFileSystem::writeInodeRegion(super_t *super, inode_t *inodes)
+{
 }
 
-int LocalFileSystem::lookup(int parentInodeNumber, string name) {
+int LocalFileSystem::lookup(int parentInodeNumber, string name)
+{
   return 0;
 }
 
-int LocalFileSystem::stat(int inodeNumber, inode_t *inode) {
+// Returns metadata of inode
+int LocalFileSystem::stat(int inodeNumber, inode_t *inode)
+{
+  // readSuperBlock in to initialize a super object
+  // Use inodebitmap to check 
+  // Use readinoderegion to populate inode 
+  // Outisde of the function in ds3ls, can get info about inode by doing inode.size
+  // you could do that, i just used stat which in turn used readsuperblock, readinodebitmap and readinoderegion
   return 0;
 }
 
-int LocalFileSystem::read(int inodeNumber, void *buffer, int size) {
+int LocalFileSystem::read(int inodeNumber, void *buffer, int size)
+{
   return 0;
 }
 
-int LocalFileSystem::create(int parentInodeNumber, int type, string name) {
+int LocalFileSystem::create(int parentInodeNumber, int type, string name)
+{
   return 0;
 }
 
-int LocalFileSystem::write(int inodeNumber, const void *buffer, int size) {
+int LocalFileSystem::write(int inodeNumber, const void *buffer, int size)
+{
   return 0;
 }
 
-int LocalFileSystem::unlink(int parentInodeNumber, string name) {
+int LocalFileSystem::unlink(int parentInodeNumber, string name)
+{
   return 0;
 }
-
--- project4/gunrock_web/ds3ls.cpp
+++ project4/gunrock_web/ds3ls.cpp
@@ -32,31 +32,33 @@ int main(int argc, char *argv[])
 
   // Utilities do not need the server--it is all local, so it will emulate the Disk and FileSystem
   Disk *disk = new Disk(argv[1], UFS_BLOCK_SIZE);
-  LocalFileSystem *fileSystem = new LocalFileSystem(disk);
+  LocalFileSystem *fileSystem = new LocalFileSystem(disk); // Use func from this after they are implemented
   string directory = string(argv[2]);
 
-  // TODO
+  // // TODO
 
-  // Go to disk and read contents of superblock (block num 0)
-  /*
-    How to read inode numbers:
-    Get the block num by: (inum + sizeof(inode_)) / blockSize
-    Then allocate a buffer that can accomodate a block
-    Then use inode index by doing inum % blockInodeCount and access it
-  */
-  int inum = 0; // Placeholder
-  int blockInodeCount = UFS_BLOCK_SIZE / sizeof(inode_t);
-  inode_t blockBuffer[blockInodeCount];
+  // // Go to disk and read contents of superblock (block num 0)
+  // /*
+  //   How to read inode numbers:
+  //   Get the block num by: (inum + sizeof(inode_)) / blockSize
+  //   Then allocate a buffer that can accomodate a block
+  //   Then use inode index by doing inum % blockInodeCount and access it
+  // */
+  // int inum = 0; // Placeholder
+  // int blockInodeCount = UFS_BLOCK_SIZE / sizeof(inode_t);
+  // inode_t blockBuffer[blockInodeCount];
 
-  int blockOffset = (sizeof(inode_t) * inum) / UFS_BLOCK_SIZE;
-  int blockNum = blockOffset + super.inode_region_addr;
+  // // super_t super;
+  // // int inodeTableStart = super.inode_region_addr;
+  // // int blockOffset = (sizeof(inode_t) * inum) / UFS_BLOCK_SIZE;
+  // // int blockNum = blockOffset + inodeTableStart;
 
-  disk->readBlock(blockNum, blockBuffer);
+  // disk->readBlock(0, blockBuffer);
 
-  int withinBlockInodeNum = inum % blockInodeCount;
-  inode_t inode = blockBuffer[withinBlockInodeNum]; // Inode for that inum retrived. Now we can get metadata from this
+  // int withinBlockInodeNum = inum % blockInodeCount;
+  // inode_t inode = blockBuffer[0]; // Inode for that inum retrived. Now we can get metadata from this
 
-  cout << inode.type;
+  // cout << inode.type;
 
   // At this point we have a number of inodes
 
